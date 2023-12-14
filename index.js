26dd911eed2b93a46271bd69acfd259bbe446fd2
emzies <112775211+emzies@users.noreply.github.com>
2023-12-14 16:17:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// You can add JavaScript functionality if needed
+// For example, smooth scrolling to section links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+     anchor.addEventListener('click', function (e) {
+         e.preventDefault();
+ 
+         document.querySelector(this.getAttribute('href')).scrollIntoView({
+             behavior: 'smooth'
+         });
+     });
+ });
+ 
